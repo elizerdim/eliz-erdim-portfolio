99cd9974d71d35458134a6018778d0100892c52c
Eliz Erdim <elizerdim@gmail.com>
2024-06-05 12:02:49 +0300

    Add linkedin href

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
   return (
-  // TODO: Add href to linkedin
     <header>
       <h1>elizerdim</h1>
       <a href="https://github.com/elizerdim" target="_blank">
         <FontAwesomeIcon icon={faGithub} />
       </a>
-      <a href="">
+      <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank">
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
     </header>
