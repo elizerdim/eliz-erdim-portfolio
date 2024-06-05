1715eec6b76ffe8d321200d0c6756197b3ce5cb0
Eliz Erdim <elizerdim@gmail.com>
2024-06-05 12:18:03 +0300

    Add aria attributes to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Header() {
   return (
     <header>
-      <h1>elizerdim</h1>
-      <a href="https://github.com/elizerdim" target="_blank">
+      <h1 aria-labelledby="name">elizerdim</h1>
+      <span id="name" className="sr-only">Eliz Erdim</span>
+      <a href="https://github.com/elizerdim" target="_blank" aria-label="Go to Eliz Erdim's GitHub page">
         <FontAwesomeIcon icon={faGithub} />
       </a>
-      <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank">
+      <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank" aria-label="Go to Eliz Erdim's LinkedIn profile">
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
     </header>
