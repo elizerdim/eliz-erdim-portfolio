4e0b9db8de831c46ff3eba18cfd71e110bf8bfe0
Eliz Erdim <elizerdim@gmail.com>
2024-06-05 17:23:27 +0300

    Add Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,22 @@
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./header.module.css"
 
 export default function Header() {
   return (
-    <header>
-      <h1 aria-labelledby="name">elizerdim</h1>
-      <span id="name" className="sr-only">Eliz Erdim</span>
-      <a href="https://github.com/elizerdim" target="_blank" aria-label="Go to Eliz Erdim's GitHub page">
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank" aria-label="Go to Eliz Erdim's LinkedIn profile">
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
+    <header className={styles.header}>
+      <div className={`container ${styles.flex}`}>
+        <h1 className="heading-m" aria-labelledby="name">elizerdim</h1>
+        <span id="name" className="sr-only">Eliz Erdim</span>
+          <div className="social-links">
+            <a href="https://github.com/elizerdim" target="_blank" aria-label="Go to Eliz Erdim's GitHub page">
+              <FontAwesomeIcon className="social-icon" icon={faGithub} />
+            </a>
+            <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank" aria-label="Go to Eliz Erdim's LinkedIn profile">
+              <FontAwesomeIcon className="social-icon" icon={faLinkedin} />
+            </a>
+          </div>
+      </div>
     </header>
   );
 }
