ecd439fbea77b3310f6d6a52f2efae8a7c7c83fa
Eliz Erdim <elizerdim@gmail.com>
2024-06-06 12:47:04 +0300

    Separate social links into its own component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,17 @@
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styles from "./header.module.css"
+import styles from "./header.module.css";
+import SocialLinks from "../SocialLinks/SocialLinks";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.flex}`}>
-        <h1 className="heading-m" aria-labelledby="name">elizerdim</h1>
-        <span id="name" className="sr-only">Eliz Erdim</span>
-          <div className="social-links">
-            <a href="https://github.com/elizerdim" target="_blank" aria-label="Go to Eliz Erdim's GitHub page">
-              <FontAwesomeIcon className="social-icon" icon={faGithub} />
-            </a>
-            <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank" aria-label="Go to Eliz Erdim's LinkedIn profile">
-              <FontAwesomeIcon className="social-icon" icon={faLinkedin} />
-            </a>
-          </div>
+        <h1 className="heading-m" aria-labelledby="name">
+          elizerdim
+        </h1>
+        <span id="name" className="sr-only">
+          Eliz Erdim
+        </span>
+        <SocialLinks />
       </div>
     </header>
   );
