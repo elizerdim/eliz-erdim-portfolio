1e871912d24eaf2b3cc12b2847147fd99b7630a0
Eliz Erdim <elizerdim@gmail.com>
2024-06-07 13:42:00 +0300

    Add Divider component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import "./App.css"
 import Hero from "./components/Hero/Hero";
+import Divider from "./components/Divider/Divider";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Divider />
     </>
   )
 }
