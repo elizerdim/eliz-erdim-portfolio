9fe833bfbb1cbec82103ce46cfe7af1a9d0403b5
Eliz Erdim <elizerdim@gmail.com>
2024-06-07 23:07:56 +0300

    Remove font-awesome package; add react-icons package

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,15 +1,14 @@
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import styles from "./social-links.module.css"
 
 export default function SocialLinks() {
   return (
     <div className={styles.links}>
       <a href="https://github.com/elizerdim" target="_blank" aria-label="Go to Eliz Erdim's GitHub page">
-        <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+        <FaGithub className={styles.icon}/>
       </a>
       <a href="https://www.linkedin.com/in/eliz-erdim/" target="_blank" aria-label="Go to Eliz Erdim's LinkedIn profile">
-        <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+        <FaLinkedin className={styles.icon} />
       </a>
     </div>
   )
