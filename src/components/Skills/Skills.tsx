2e022a19c2e25dbea7866787c2a2f9a2328b6b1c
Eliz Erdim <elizerdim@gmail.com>
2024-06-08 23:13:53 +0300

    Change ES6's icon

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,7 +2,7 @@ import skills from "../../data/skills.json";
 import { FaReact, FaHtml5, FaCss3Alt } from "react-icons/fa";
 import { SiTypescript, SiJavascript } from "react-icons/si";
 import { IoAccessibility } from "react-icons/io5";
-import { PiFileCodeLight } from "react-icons/pi";
+import { PiFileCode } from "react-icons/pi";
 import { BiGridVertical } from "react-icons/bi";
 import { BsGrid3X2 } from "react-icons/bs";
 import { LuCode2 } from "react-icons/lu";
@@ -19,7 +19,7 @@ const icons: iconsObj = {
   SiTypescript,
   SiJavascript,
   IoAccessibility,
-  PiFileCodeLight,
+  PiFileCode,
   BiGridVertical,
   BsGrid3X2,
   LuCode2,
