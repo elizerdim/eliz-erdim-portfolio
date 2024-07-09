302170eeda4f1febbfe4f5a2d4ff999182103d0b
Eliz Erdim <elizerdim@gmail.com>
2024-07-09 15:49:38 +0300

    Add Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
-// import styles from "./footer.module.css"
+import SocialLinks from "../SocialLinks/SocialLinks"
+import styles from "./footer.module.css"
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <div className="container">
+        <h5 className={`${styles.heading} heading-xl`}>Contact</h5>
+        <p className={`${styles.text} body-l`}>You can also check out my GitHub and LinkedIn accounts. Send me an email if you are interested in working together!</p>
+        <div className={styles.links}>
+          <address className={styles.email}>
+            <a href="mailto:elizerdim@gmail.com">elizerdim@gmail.com</a>
+          </address>
+          <SocialLinks />
+        </div>
+        <hr />
+      </div>
+    </footer>
   )
 }
