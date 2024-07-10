b0caa1bee2a2c851a8571f1af13a684f9d103570
Eliz Erdim <elizerdim@gmail.com>
2024-07-10 12:47:19 +0300

    Make social links bigger for smaller screens and adjust related elements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.flex}`}>
-        <h1 className="heading-m" aria-labelledby="name">
+        <h1 className={styles.name} aria-labelledby="name">
           elizerdim
         </h1>
         <span id="name" className="sr-only">
